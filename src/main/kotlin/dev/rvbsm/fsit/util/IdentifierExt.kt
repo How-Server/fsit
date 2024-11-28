853265bad4846975386016f4c050ee03548c0e24
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2024-11-28 16:20:36 +0200

    refactor(stonecutter): remove unnecessary chisel in default `Identifier`

--- src/main/kotlin/dev/rvbsm/fsit/util/IdentifierExt.kt
+++ src/main/kotlin/dev/rvbsm/fsit/util/IdentifierExt.kt
@@ -2,12 +2,7 @@ package dev.rvbsm.fsit.util
 
 import net.minecraft.util.Identifier
 
-val DEFAULT_IDENTIFIER: Identifier =
-    //? if <=1.20.6
-    Identifier("air")
-    //? if >=1.21
-    /*Identifier.ofVanilla("air")*/
-
+val DEFAULT_IDENTIFIER = Identifier.of(Identifier.DEFAULT_NAMESPACE, "air") as Identifier
 fun Identifier?.orDefault(): Identifier = this ?: DEFAULT_IDENTIFIER
 
 fun String.id(namespace: String = Identifier.DEFAULT_NAMESPACE) = runCatching {
